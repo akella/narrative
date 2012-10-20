aa5c1443c109bb2f9f6d617da631f7e36d15c2dc
akella <akella.a@gmail.com>
2012-10-20 03:47:04 +0300

    some hovers+scripts

--- js/common.js
+++ js/common.js
@@ -1,78 +1,39 @@
-
-$.fn.scrollableAddClones = function(addItems) {
-    // grab scrollable plugin
-    var scrollable;
-    if (!(scrollable = $(this).data('scrollable')) || !scrollable.getConf().circular)
-    return;
-    // grab scrollable elements and remember it's count
-    var nodes = scrollable.getItems();
-    var length = nodes.length;
-
-    // grab class for the nodes
-    var clonedClass = scrollable.getConf().clonedClass;
-    // get wrap object to append the clones to
-    var wrap = scrollable.getItemWrap();
-    // fill as much nodes as needed for 500 pixels
-    if (!addItems) addItems = Math.ceil(1000 / nodes.eq(1).width());
-    // create fake container to add the clones to (max 15 clones)
-    var newNodesAppend = $('<span />');
-    for (var i = 1; i <= (addItems < 15 ? addItems : 15); i++)
-    nodes.eq(i % length).clone().addClass(clonedClass).appendTo(newNodesAppend);
-    // insert HTML
-    newNodesAppend.children().appendTo(wrap);
-}
-
 $(document).ready(function() {
 	
-	if($('.room__caroin').length>0){
-		asd = $('.room__caroin').scrollable({
-			next:'.room__next',
-			prev:'.room__prev',
-			circular:true,
-			api:true
-		});
 
-		asd.onSeek(function(event, index) {
-			var currentItem = this.getItems().eq(this.getIndex());
-			$('.room__slide').removeClass('room__slide_active');
-			currentItem.addClass('room__slide_active');
-		});
-		$(".room__caroin").scrollableAddClones();
+
+$('.slider').each(function(){
+	show = $(this);
+	prev = show.children('.slider__prev');
+	next = show.children('.slider__next');
+	slider = show.children('.slider__slides');
+	slidecount = false;
+	if(show.children('.slider__info').length>0){
+		slidecount = show.children('.slider__info');
 	}
 
-	$('.lookevent__caro').scrollable({
-		next:'.lookevent__next',
-		prev:'.lookevent__prev',
-		circular:true
-	});
-	$(".lookevent__caro").scrollableAddClones();
-
-	
-	$('.nav>li>a').click(function() {
-		if($(this).hasClass('nav__active')){
-			$(this).removeClass('nav__active').next().hide();
-		}
-		else{
-			$('.nav>li>a').removeClass('nav__active');
-			$('.nav__drop').hide();
-			$(this).addClass('nav__active');
-			$(this).next().show();
-		}
-		return false;
+	slider.cycle({
+	    fx:     'scrollHorz',
+	    speed:  'fast',
+	    timeout: 0,
+	    next: show.children('.slider__next'),
+	    prev: show.children('.slider__prev'),
+	    after: function (curr,next,opts) {
+	    		go = $(curr).closest('.slider').children('.slider__info');
+				go.html((opts.currSlide + 1) + " / " + opts.slideCount + "");
+				var index = opts.currSlide;
+				var $ht = $(this).height();
+				//set the container's height to that of the current slide
+				$(this).parent().css("height", $ht);
+	    }
 
 	});
-	
-	$('.h-you__drop').click(function() {
-		$('.h-you__dropblock').fadeToggle();
-		return false;
-	});
-	
-	$('.select,.size').click(function() {
-		$(this).children('.dropdown').fadeToggle();
-		$(this).toggleClass('select_active');
-	});
-	
-	$('.promo__wrap').scrollable({vertical:true}).navigator(".promo__nav");
+
+	function onAfter(curr,next,opts) {
+		  
+	}
+
+})
 
 
 
