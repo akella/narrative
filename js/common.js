c40d7367d03ee61fccfe0d265b1384fdf7a0c54a
mirantel <magi.mirantel@gmail.com>
2012-10-30 15:37:02 +0200

    discover

--- js/common.js
+++ js/common.js
@@ -1,8 +1,70 @@
 $(document).ready(function() {
 	
+	// function element exists
+	jQuery.fn.exists = function() {
+		return $(this).length;
+	}
+
 	$('.user-more-arr').click(function(){
-		$(this).siblings('.user-inf').fadeIn('fast');
+		$(this).siblings('.user-inf_popup').fadeToggle('fast');
 	});
+
+	$('html').click(function(e) {
+		if($('.user-inf_popup').is(":visible")){ 
+			if (e.target.class != 'user-inf_popup' && $(e.target).parents('.user-inf_popup').length == 0) {
+				$('.user-inf_popup').fadeOut();
+			}
+		}
+
+		if ($('.icon_comments').hasClass("icon_active")){
+			if (e.target.class != 'notif' && e.target.class != 'icon_comments' && $(e.target).parents('.notif').length == 0) {
+				$('.notif').fadeOut();
+				$('.icon_comments').removeClass('icon_active');
+			}
+		}
+	});
+
+	if ($('.popup').exists()){
+		var win = $(window);
+		var popup = $('.popup');
+		var w = win.height();
+		var p = popup.height() + parseInt(popup.css('top'));
+		$(window).resize(function(){
+			w = win.height();
+			if (w >= p){
+				popup.css('position','fixed');
+			}
+			else{
+				popup.css('position','absolute');
+				win.scroll(function(){
+				var wst = win.scrollTop();
+					if(wst + w >= p+30){ 
+					popup.css('position','fixed').addClass('popup_bottom');
+				}
+				else{
+					popup.css('position','absolute').removeClass('popup_bottom');
+				}
+				});
+			}
+		});
+
+		if (w >= p){
+			popup.css('position','fixed');
+		}
+		else{
+			popup.css('position','absolute');
+			win.scroll(function(){
+			var wst = win.scrollTop();
+				if(wst + w >= p+30){ 
+					popup.css('position','fixed').addClass('popup_bottom');
+				}
+				else{
+					popup.css('position','absolute').removeClass('popup_bottom');
+				}
+			});
+		}
+	};
+
 
 	$('input.check').css('opacity', '0').wrap("<div class='checkbox'></div>")
 		.delegate($(this), 'change', function(){ $(this).attr('checked') ? $(this).parent().parent().addClass('checked') : $(this).parent().parent().removeClass('checked')});
@@ -26,6 +88,22 @@ $(document).ready(function() {
 		$(this).attr('placeholder',plh);
 	});
 
+	$('.icon_comments').click(function(){
+		var notif = $('.notif')
+		if ($(this).hasClass('icon_active')){
+			notif.fadeOut('fast');
+			notif.find('.mess__author').removeClass('notif_animation');
+			notif.find('.mess-list__arr').removeClass('notif_animation');
+			$(this).removeClass('icon_active');
+		}
+		else{
+			notif.fadeIn('fast');
+			notif.find('.mess__author').addClass('notif_animation');
+			notif.find('.mess-list__arr').addClass('notif_animation');
+			$(this).addClass('icon_active');
+		}
+		
+	});
 
 
 $('.slider').each(function(){
